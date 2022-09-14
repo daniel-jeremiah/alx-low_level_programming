db0e7e3511ff4ee74bd624d7f8929daaf1a8b44d
daniel-jeremiah <cy63daniel@gmail.com>
2022-09-13 20:24:32 -0700

    fly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,15 @@
 
 /**
  * main - entry point                                                       
- *                                                                                              
- * Return: always 0 (Success)                                                                                     
-  */
+ *                                               
+ * Return: always 0 (Success)                                             
+ */
 
 int main(void)
-
 {
   
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
       
