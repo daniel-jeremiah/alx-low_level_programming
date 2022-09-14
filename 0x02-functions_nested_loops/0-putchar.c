8dd6d53779545c83f81e82f2c5f324a9a3a86b15
daniel-jeremiah <cy63daniel@gmail.com>
2022-09-13 19:43:07 -0700

    had

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,26 +8,18 @@
 
 
 
-int main(void)
-  
+int main(void) 
 {
+        int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
   
-  int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
   
-  int count, sz;
-  
-
-  
-  sz = sizeof(str) / sizeof(int);
-  
-  for (count = 0; count < sz; count++)
-    
-    {
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
       
-      _putchar(str[count]);
-      
-    }
-  
-  _putchar('\n');
-  
-        return 
+                   _putchar(str[count])
+	}
+	_putchar('\n');
+        return (0);
+}
