217097e12926688e0b56c333dae9ca94d76d61b3
daniel-jeremiah <cy63daniel@gmail.com>
2022-09-13 19:45:51 -0700

    had

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,11 @@
  * Return: Always 0 (Success)                               
  */
 
-
-
 int main(void) 
 {
         int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
-  
-  
+    
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
